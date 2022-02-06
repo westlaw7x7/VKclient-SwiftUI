79ccbf48e141bc83b25d708789907a38e2e9fb78
Alexander <grigoryev_7x7@icloud.com>
2022-02-06 18:34:42 +0300

    The project was created

---
+++ VKclient-SwiftUI/VKclient-SwiftUI/VKclient_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VKclient_SwiftUIApp.swift
+//  VKclient-SwiftUI
+//
+//  Created by Alexander Grigoryev on 06.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct VKclient_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
